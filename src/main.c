bfc54e95bf22ad907ea1e4cd07905dea5e701414
peraza97 <omarperaza19@gmail.com>
2018-02-17 22:52:48 -0800

    ehh

--- src/main.c
+++ src/main.c
@@ -6,28 +6,21 @@
 #include "./../headers/scheduler.h"
 #include "./../headers/adc.h"
 #include "./../headers/joystick.h"
+#include "./../headers/queue.h"
 
 //gobal variables
 //stores the display message for each gear
-unsigned char * speed[] = {"Gear 1, Gear 2, Gear 3, Gear 4"};
+unsigned char * speed[] = {"Gear 1", "Gear 2", "Gear 3","Gear 4"};
 //stores the currentGear of the tranmission
 unsigned char currentGear = 0x00;
 //tracks if the car is on or not
 unsigned char Ignition = 0x00;
-//shifting boolean
+//am i currently shifting
 unsigned char shifting = 0x00;
-//grab the input
-unsigned char direction = 0x00;
 
-//JOYSTICK TICK FUNCTION
-enum JoyStick_States {Joystick_Start, Wait, Input} joystick_State;
-
-int JoystickTick(int state){
-    return state;
-}
+Queue shiftList;
 
 //Motor TICK FUNCTION
-
 enum Motor_States {Motor_Start, Off, On, On_Hold, Off_Hold} motor_State;
 
 int MotorTick(int state){
@@ -35,11 +28,12 @@ int MotorTick(int state){
     switch (state) {
         case Motor_Start: //MOTOR START STATE
             state = Off;
+            LCD_DisplayString(1,"Motor Off");
             break;
         case Off: //MOTOR OFF STATE
             if(button){
                 state = On_Hold;
-                LCD_DisplayString(1,"Turned On");
+                LCD_DisplayString(1,speed[currentGear]);
             }
             else{
                 state = Off;
@@ -55,7 +49,7 @@ int MotorTick(int state){
             break;
         case On: //On STATE
             if(button){
-                LCD_DisplayString(1,"Turned Off");
+                LCD_DisplayString(1,"Motor Off");
                 state = Off_Hold;
             }
             else{
@@ -88,6 +82,88 @@ int MotorTick(int state){
     return state;
 }
 
+//JOYSTICK TICK FUNCTION
+enum JoyStick_States {Joystick_Start, Wait, Input} joystick_State;
+
+int JoystickTick(int state){
+    unsigned char stick = getJoystick();
+    switch (state) {
+        case Joystick_Start: //start state
+            state = Wait;
+            break;
+        case Wait: //wait state
+            if(Ignition && stick && !shifting){
+                state = Input; //go to input amd push it onto the queue
+                QueueEnqueue(shiftList,stick);
+            }
+            else{
+                state = Wait;
+            }
+            break;
+        case Input: // Input state
+            if(Ignition && stick){
+                state = Input;
+            }
+            else{
+                state = Wait;
+            }
+            break;
+        default:
+            state = Wait;
+            break;
+    }
+    return state;
+}
+
+//SHIFTER TICK FUNCTION
+enum Shifter_States {Shifter_Start, Shifter_Wait, Shifter_Shift} shifter_State;
+
+int ShifterTick(int state){
+    
+    switch (state) {
+        case Shifter_Start: //start state
+            state = Shifter_Wait;
+            break;
+        case Shifter_Wait: //wait states
+            if(!QueueIsEmpty(shiftList)){
+                state = Shifter_Shift;
+                shifting = 1;
+            }
+            else{
+                state = Shifter_Wait;
+            }
+            break;
+        case Shifter_Shift: //shifting
+            state = Shifter_Wait;
+            shifting = 0;
+            break;
+        default:
+            state = Shifter_Wait;
+            break;
+    }
+    switch (state) {
+        case Shifter_Shift: ;
+            unsigned char g = QueueDequeue(shiftList);
+            //call some function to get to that gear
+            if(g == 1){
+                currentGear = currentGear >= 3 ? 3: currentGear + 1;
+                
+            }
+            else{
+                currentGear = currentGear > 0 ? currentGear - 1: 0;
+            }
+            LCD_DisplayString(1,speed[currentGear]);
+            break;
+        default:
+            break;
+    }
+    
+    return state;
+}
+
+
+
+
 int main(void){
     //Joystick Register/Button Register
     DDRA = 0x00; PORTA = 0xFF;
@@ -99,21 +175,27 @@ int main(void){
     LCD_init();
     LCD_ClearScreen();
     
-    static task task1, task2;
-    task *tasks[] = {&task1,&task2};
+    //MY QUEUE
+    shiftList = QueueInit(4);
+    
+    static task task1, task2, task3;
+    task *tasks[] = {&task1,&task2, &task3};
     const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
     
     // Period for the tasks
-    unsigned long int SMTick1_calc = 100;
-    unsigned long int SMTick2_calc = 100;
+    unsigned long int SMTick1_calc = 200;
+    unsigned long int SMTick2_calc = 200;
+    unsigned long int SMTick3_calc = 200;
     //Calculating GCD
     unsigned long int tmpGCD = 1; // Normally initialized to 1
     tmpGCD = findGCD(SMTick1_calc, SMTick2_calc);
+    tmpGCD = findGCD(tmpGCD, SMTick3_calc);
     //set the GCD
     unsigned long int GCD = tmpGCD;
     //Recalculate GCD periods for scheduler
     unsigned long int Motor_period = SMTick1_calc/GCD;
     unsigned long int Joystick_period = SMTick2_calc/GCD;
+    unsigned long int Shifter_period = SMTick3_calc/GCD;
     //Motor Task
     task1.state = Motor_Start;//Task initial state.
     task1.period = Motor_period;//Task Period.
@@ -125,6 +207,12 @@ int main(void){
     task2.period = Joystick_period;//Task Period.
     task2.elapsedTime = Joystick_period;//Task current elapsed time.
     task2.TickFct = &JoystickTick;//Function pointer for the tick.
+    
+    //Shifter Task
+    task3.state = Shifter_Start;//Task initial state.
+    task3.period = Shifter_period;//Task Period.
+    task3.elapsedTime = Shifter_period;//Task current elapsed time.
+    task3.TickFct = &ShifterTick;//Function pointer for the tick.
     
     TimerSet(GCD);
     TimerOn();
